cd4e10074a28dca0096d22594abab0076033b3e3
Lyquocminh <88130401+Lyquocminh@users.noreply.github.com>
2022-08-26 14:32:22 +0700

    Update 1_4_cau3.cpp

--- Lab_1/1_4_cau3.cpp
+++ Lab_1/1_4_cau3.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 struct Date{
 	int ngay,thang,nam;
 };
@@ -26,25 +26,53 @@ int SoSanhDate(Date a, Date b){
 	if(a.nam!=b.nam) return 0;
 	return 1;
 }
-int TimHocSinh(HocSinh hs[], int n, Date x){
+int TimTheoMaSo(HocSinh hs[], int n, int x){
+	for(int i=0;i<n;i++)
+		if(hs[i].MS == x)
+			return i;
+	return -1;
+}
+int TimTheoNgaySinh(HocSinh hs[], int n, Date x){
 	for(int i=0;i<n;i++)
 		if(SoSanhDate(hs[i].NgaySinh, x))
 			return i;
 	return -1;
+}
+int TimTheoTen(HocSinh hs[], int n, char x[]){
+	for(int i=0; i<n; i++)
+		if(strcmp(hs[i].ten, x)==0)
+			return i;
+	return -1;
+}
+void XuatSV(HocSinh hs){
+	printf("\nMa hoc sinh: %d", hs.MS);
+	printf("\nHo va ten: %s", hs.ten);
+	printf("\nNgay sinh: %d/%d/%d", hs.NgaySinh.ngay, hs.NgaySinh.thang, hs.NgaySinh.nam);
+}
+void XuatKQ(HocSinh hs[], int kq){
+	if(kq!=-1)
+		XuatSV(hs[kq]);
+	else
+		printf("\nKhong tim thay");
 }
 int main(){
 	int n;
 	printf("Nhap vao so luong hoc sinh: "); scanf("%d", &n);
 	HocSinh hs[n];
 	NhapDSHS(hs,n);
+	int id;
+	printf("\nNhap vao ma hs can tim: "); scanf("%d", &id);
+	int kq = TimTheoMaSo(hs,n,id);
+	XuatKQ(hs, kq);
+	char name[20];
+	fflush(stdin);
+	printf("\nNhap vao ten hoc sinh can tim: "); gets(name);
+	kq = TimTheoTen(hs,n,name);
+	XuatKQ(hs, kq);
 	Date x;
-	printf("Nhap vao ngay sinh can tim (ngay/thang/nam): ");
+	printf("\nNhap vao ngay sinh can tim (ngay/thang/nam): ");
 	scanf("%d/%d/%d", &x.ngay, &x.thang, &x.nam);
-	int kq= TimHocSinh(hs, n, x);
-	if(kq!=-1){
-		printf("\nHoc sinh co ngay sinh %d/%d/%d \n", x.ngay, x.thang, x.nam);
-		printf("Ho ten: %s \t Ma hoc sinh: %d", hs[kq].ten, hs[kq].MS);
-	}else
-		printf("\nKhong tim thay hoc sinh co ngay sinh %d/%d/%d \n", x.ngay, x.thang, x.nam);
+	kq= TimTheoNgaySinh(hs, n, x);
+	XuatKQ(hs, kq);
 	return 0;
 }
